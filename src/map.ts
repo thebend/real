50b3acbc227a689c177ed5169dc8785ea978a0e0
Benjamin Davidson <ben.davidson@gmail.com>
2017-03-20 11:33:44 -0700

    Node build scripts for TypeScript

--- src/map.ts
+++ src/map.ts
@@ -1,12 +1,10 @@
-//// <reference path="types.d.ts" />
-// import 'core-js/library/es6/array';
-// import {LandProperty, Zone} from './types';
-// import * as d3 from './d3-bundle';
-
 // polyfills
 Array.prototype.find||Object.defineProperty(Array.prototype,"find",{value:function(a){if(null==this)throw new TypeError('"this" is null or not defined');var b=Object(this),c=b.length>>>0;if("function"!=typeof a)throw new TypeError("predicate must be a function");for(var d=arguments[1],e=0;e<c;){var f=b[e];if(a.call(d,f,e,b))return f;e++}}});
+Array.prototype.includes||Object.defineProperty(Array.prototype,"includes",{value:function(a,b){function g(a,b){return a===b||"number"==typeof a&&"number"==typeof b&&isNaN(a)&&isNaN(b)}if(null==this)throw new TypeError('"this" is null or not defined');var c=Object(this),d=c.length>>>0;if(0===d)return!1;for(var e=0|b,f=Math.max(e>=0?e:d-Math.abs(e),0);f<d;){if(g(c[f],a))return!0;f++}return!1}});
+
 interface Array<T> {
     find(predicate: (needle: T) => boolean): T;
+    includes(needle: T): boolean
 }
 
 interface Zone {
@@ -404,7 +402,7 @@ $(function() {
         d.land_glyph = getGlyph(d.previous_land, d.total_assessed_land);
         d.building_glyph = getGlyph(d.previous_building, d.total_assessed_building);
         d.total_glyph = getGlyph(d.previous_total, d.total_assessed_value);
-        d.zone = zones.find(z => z.codes.indexOf(d.zoning) > -1);
+        d.zone = zones.find(z => z.codes.includes(d.zoning));
         return d;
     }).get(function(error, data: LandProperty[]) {
         mapUi.setData(data);
